ef5078e9ba1180049f04272d24158154eb994021
Камышенков Артем Сергеевич <kamishenkov@innovasystem.pro>
2023-08-03 22:42:38 +0300

    add start layout

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,31 @@
 import cn from 'classnames';
+import { Layout, Menu } from 'antd';
 import { useTheme } from '@/app/providers/themeProvider';
-import './styles/index.scss';
 import AppRouter from '@/app/providers/router/ui/AppRouter';
+import './styles/index.scss';
 
 export const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className={cn('app', theme)}>
-      <AppRouter />
-      <h1>App</h1>
-      <button type="button" onClick={toggleTheme}>
-        toggle
-      </button>
-    </div>
+    <Layout className={cn('app', theme)}>
+      <Layout.Sider width={200} style={{ background: 'black' }}>
+        <Menu
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          style={{ height: '100%', borderRight: 0 }}
+          items={[]}
+        />
+      </Layout.Sider>
+
+      <Layout.Content>
+        <AppRouter />
+        <h1>App</h1>
+        <button type="button" onClick={toggleTheme}>
+          toggle
+        </button>
+      </Layout.Content>
+    </Layout>
   );
 };
