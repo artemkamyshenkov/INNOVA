4a04a416f2a480c2514ef064ec166b0ce2166139
Камышенков Артем Сергеевич <kamishenkov@innovasystem.pro>
2023-08-07 22:01:26 +0300

    add sidebar

--- src/widgets/Sidebar/ui/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar.tsx
@@ -1,6 +1,31 @@
-import React from 'react';
+import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
+import { Button, Menu } from 'antd';
+import { useState } from 'react';
+import { sidebarItems } from './sidebarItems';
 import styles from './Sidebar.module.scss';
 
-export const Sidebar = () => (
-  <div className={styles.autoprefixerTest}>Sidebar</div>
-);
+export const Sidebar = () => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const toggleCollapsed = () => {
+    setCollapsed(!collapsed);
+  };
+
+  return (
+    <>
+      <Button
+        type="primary"
+        onClick={toggleCollapsed}
+        style={{ marginBottom: 16 }}
+      >
+        {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+      </Button>
+      <Menu
+        mode="inline"
+        inlineCollapsed={collapsed}
+        items={sidebarItems()}
+        className={styles.sidebar}
+      />
+    </>
+  );
+};
