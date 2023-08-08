eb8aeb0a12ad3428f57bdc5bcd1279e99633fc4a
Камышенков Артем Сергеевич <kamishenkov@innovasystem.pro>
2023-08-08 23:17:04 +0300

    create redux

---
+++ src/app/providers/storeProvider/config/StateSchema.ts
@@ -0,0 +1,26 @@
+import {
+  AnyAction,
+  CombinedState,
+  EnhancedStore,
+  Reducer,
+  ReducersMapObject,
+} from '@reduxjs/toolkit';
+import { UserSchema } from '@/entities/User';
+
+export interface StateSchema {
+  user: UserSchema;
+  // Async redusers
+}
+
+export type StateSchemaKeys = keyof StateSchema;
+
+export interface ReducerManager {
+  getReducerMap: () => ReducersMapObject<StateSchema>;
+  reduce: (state: StateSchema, action: AnyAction) => CombinedState<StateSchema>;
+  add: (key: StateSchemaKeys, reducer: Reducer) => void;
+  remove: (key: StateSchemaKeys) => void;
+}
+
+export interface ReduxStoreWithManager extends EnhancedStore<StateSchema> {
+  reducerManager: ReducerManager;
+}
--- src/screens/MainPage/ui/MainPage.tsx
+++ src/screens/MainPage/ui/MainPage.tsx
@@ -1,5 +1,16 @@
-import React from 'react';
+import { redirect, useNavigate } from 'react-router-dom';
+import { useAppSelector } from '@/shared/hooks/redux';
 
-const MainPage = () => <div>Main</div>;
+const MainPage = () => {
+  const { authData } = useAppSelector(state => state.user);
+  const navigane = useNavigate();
+  if (authData?.id) {
+    return <div>Main</div>; // Возвращаем JSX-элемент
+  }
+  redirect('/login');
+  return null;
+
+  // return authData?.id ? <div>Main</div> : redirect('/login');
+};
 
 export default MainPage;
